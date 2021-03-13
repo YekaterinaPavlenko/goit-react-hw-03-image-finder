a1ca14e47bf5df44ef9dec2dded982d403e31ed1
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-03-13 23:38:33 +0200

    Create ImageGallery.js

---
+++ src/components/ImageGallery/ImageGallery.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import igs from "./ImageGallery.module.css";
+import PropTypes from "prop-types";
+import ImageGalleryItem from "./ImageGalleryItem/ImageGalleryItem";
+
+class ImageGallery extends Component {
+  state = {};
+  render() {
+    const { gallery } = this.props;
+    return (
+      <ul className={igs.ImageGallery}>
+        {gallery.map((item) => {
+          //   console.log(item);
+          const { id, webformatURL, tags } = item;
+          return <ImageGalleryItem key={id} image={webformatURL} tags={tags} />;
+        })}
+      </ul>
+    );
+  }
+}
+ImageGallery.propTypes = {
+  gallery: PropTypes.arrayOf(PropTypes.shape()).isRequired,
+  //   deleteContact: PropTypes.func.isRequired,
+};
+export default ImageGallery;
