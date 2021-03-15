5633f27908026237de8d4c58690bbee7c6fa1c51
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-03-15 17:45:38 +0200

    Update pixabayApi.js

--- src/services/pixabayApi.js
+++ src/services/pixabayApi.js
@@ -1,41 +1,18 @@
-const apiKey = "19851067-7c860a6e23d51e90a164e5364";
-
-// export default class ImageApiService {
-//   constructor({ query, page }) {
-//     this.searchQuery = query;
-//     this.page = page;
-//     this.apiKey = "19851067-7c860a6e23d51e90a164e5364";
-//   }
+const apiKey = '19851067-7c860a6e23d51e90a164e5364';
 
 function fetchImages(searchQuery, page) {
-  console.log(searchQuery);
+  // console.log(searchQuery);
   const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&page=${page}&per_page=12`;
 
   return fetch(url)
-    .then((response) => {
+    .then(response => {
       // console.log(response);
       return response.json();
     })
     .then(({ hits }) => {
       // console.log({ hits });
-      // this.changePage();
       return hits;
     });
 }
 
-// changePage() {
-//   this.page += 1;
-// }
-
-// resetPage() {
-//   this.page = 1;
-// }
-
-// get query() {
-//   return this.searchQuery;
-// }
-// set query(newQuery) {
-//   this.searchQuery = newQuery;
-// }
-// }
 export default fetchImages;
