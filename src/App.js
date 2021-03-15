a12ba0798fc14b6818f28abd18f9e8687ecab337
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-03-15 17:47:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,103 +1,118 @@
-import React, { Component } from "react";
-import "./App.css";
-import Searchbar from "./components/Searchbar/Searchbar";
-import fetchImages from "./services/pixabayApi";
-import ImageGallery from "./components/ImageGallery/ImageGallery";
-import Button from "./components/Button/Button";
+import React, { Component } from 'react';
+import './App.css';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import Loader from 'react-loader-spinner';
+import Searchbar from './components/Searchbar/Searchbar';
+import fetchImages from './services/pixabayApi';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Button from './components/Button/Button';
 
 class App extends Component {
   state = {
-    query: "",
+    query: '',
     page: 1,
     gallery: [],
+    fetchLength: null,
+    isLoading: false,
   };
   getQueryByForm = ({ query }) => {
     console.log(query);
-    this.setState({ query });
+    this.setState({ query: query, page: 1, gallery: [] });
   };
-  setNewPage = (page) => {
+  setNewPage = e => {
+    e.preventDefault();
+    const { page } = this.state;
     console.log(page);
-    this.setState({ page: page });
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
   };
-  // formSubmitHandler = (data) => {
-  //   const { contacts } = this.state;
-  //   let existName = contacts.find(
-  //     (contact) => contact.name.toLowerCase() === data.name.toLowerCase()
-  //   );
-  //   let existNumber = contacts.find(
-  //     (contact) => contact.number.toLowerCase() === data.number.toLowerCase()
-  //   );
-  //   let existContact = (existName && "name") || (existNumber && "number");
-  //   // console.log(existContact);
 
-  //   existName || existNumber
-  //     ? alert(`The ${existContact} is already in contacts.`)
-  //     : this.setState((prevState) => ({
-  //         contacts: [data, ...prevState.contacts],
-  //       }));
-  // };
-
-  // addFilterValue = (e) => {
-  //   this.setState({ filter: e.currentTarget.value });
-  // };
-  // getVisibleContacts = () => {
-  //   const { filter, contacts } = this.state;
-  //   // console.log(filter);
-  //   const normalizedFilter = filter.toLowerCase();
-
-  //   return contacts.filter((contact) =>
-  //     contact.name.toLowerCase().includes(normalizedFilter)
-  //   );
-  // };
-  // onDeleteContact = (id) => {
-  //   const { contacts } = this.state;
-  //   this.setState({
-  //     contacts: contacts.filter((contact) => contact.id !== id),
-  //   });
-  // };
-  componentDidMount() {
-    console.log("я родился");
-    const { query, page } = this.state;
-    // console.log(query);
-    fetchImages(query, page).then((result) => {
-      // console.log(result);
-      this.setState({ gallery: [...result] });
-    });
-  }
+  // componentDidMount() {
+  //   console.log('я родился');
+  //   // console.log(query);
+  //   const { query } = this.state;
+  //   if (query) {
+  //     this.fetchImages(query);
+  //   }
+  // }
   componentDidUpdate(prevProps, prevState) {
-    console.log("я обновился");
+    console.log('я обновился');
     // console.log(prevState);
     // console.log(this.state);
     const { query, page } = this.state;
     if (query !== prevState.query) {
-      console.log(query);
-      fetchImages(query, page).then((result) => {
-        console.log(query);
-        console.log(result);
-        this.setState({ gallery: [...result] });
-      });
+      this.fetchImagesByQuery(query);
     } else if (query === prevState.query && page !== prevState.page) {
-      console.log(query);
-      console.log(page);
-      fetchImages(query, page).then((result) => {
-        console.log(query);
-        console.log(result);
-        this.setState({ gallery: [...prevState.gallery, ...result] });
-      });
+      this.fetchImagesByQuery(query);
     }
   }
 
-  render() {
-    const { gallery, page } = this.state;
+  fetchImagesByQuery = (prevProps, prevState) => {
+    const { query, page } = this.state;
+    // console.log(query);
+    // console.log(page);
+    // this.setState({ isLoading: true });
+
+    // console.log(query);
+    // fetchImages(query, page)
+    //   .then(result => {
+    //     console.log(query);
+    //     console.log(result);
+    //     this.setState({ gallery: [...result], fetchLength: result.length });
+    //   })
+    //   .catch(error => console.log(error))
+    //   .finally(() => {
+    //  this.setState({ isLoading: false });
+    //     window.scrollTo({
+    //       top: document.documentElement.scrollHeight,
+    //       behavior: 'smooth',
+    //     });
+    //   });
+    //  if (query === prevState.query && page !== prevState.page) {
+    console.log(query);
     console.log(page);
+    this.setState({ isLoading: true });
+    fetchImages(query, page)
+      .then(result => {
+        console.log(query);
+        console.log(result);
+        this.setState(prevState => ({
+          gallery: [...prevState.gallery, ...result],
+          fetchLength: result.length,
+        }));
+      })
+      .catch(error => console.log(error))
+      .finally(() => {
+        this.setState({ isLoading: false });
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
+        });
+      });
+    // }
+  };
+  render() {
+    const { gallery, fetchLength, isLoading } = this.state;
+    console.log(fetchLength);
     // const visibleContacts = this.getVisibleContacts();
     return (
       <div className="App">
         <h1>Search image</h1>
         <Searchbar onSubmit={this.getQueryByForm} />
-
         <ImageGallery gallery={gallery} />
-        <Button page={page} getNewPage={this.setNewPage} />
+        {isLoading && (
+          <Loader
+            type="ThreeDots"
+            color="#3f51b5"
+            height={100}
+            width={100}
+            timeout={3000} //3 secs
+          />
+        )}
+        {fetchLength === 12 && !isLoading && (
+          <Button getNewPage={this.setNewPage} />
+        )}
       </div>
     );
   }
