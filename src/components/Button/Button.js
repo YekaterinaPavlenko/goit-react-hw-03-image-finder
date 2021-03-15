f873ef5dc99833ceed0083602249ca49b681333d
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-03-15 17:47:18 +0200

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,50 +1,22 @@
-import React, { Component } from "react";
-import bs from "./Button.module.css";
-// import PropTypes from "prop-types";
+import React from 'react';
+import bs from './Button.module.css';
+import PropTypes from 'prop-types';
 
-export default class Button extends Component {
-  state = {
-    page: this.props.page,
-    active: false,
-  };
-  changePage = (e) => {
-    e.preventDefault();
-    const { page } = this.props;
-    console.log(page);
-    this.setState((prevState) => {
-      console.log(prevState);
-      return {
-        page: +prevState.page + 1,
-      };
-    });
-  };
-  addNewPage = (e) => {
-    e.preventDefault();
-    const { getNewPage } = this.props;
-    const { page } = this.state;
-    console.log(page);
-
-    getNewPage(page);
-  };
-  //   handleSubmit = (e) => {
-  //     e.preventDefault();
-  //     this.props.onSubmit({ id: uuidv4(), ...this.state });
-  //     this.reset();
-  //   };
-  render() {
-    return (
-      <button
-        className={bs.Button}
-        type="button"
-        data-action="load-more"
-        // disabled={true}
-        onClick={(e) => {
-          this.changePage(e);
-          this.addNewPage(e);
-        }}
-      >
-        Show more
-      </button>
-    );
-  }
-}
+const Button = ({ getNewPage }) => {
+  // console.log(getNewPage);
+  return (
+    <button
+      className={bs.Button}
+      type="button"
+      data-action="load-more"
+      // disabled={true}
+      onClick={getNewPage}
+    >
+      Show more
+    </button>
+  );
+};
+Button.propTypes = {
+  getNewPage: PropTypes.func.isRequired,
+};
+export default Button;
