2d706ffa5ae1000fae7798afb7802a0147df51fe
YekaterinaPavlenko <pavluka1605@gmail.com>
2021-03-16 11:27:10 +0200

    add  modal marcup

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import ms from './Modal.module.css';
+// import PropTypes from 'prop-types';
+const Modal = () => {
+  return (
+    <div className="Overlay">
+      <div className="Modal">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
+export default Modal;
